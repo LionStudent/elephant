57421c08b9dc4c19bc5e44930d973696bbd6586d
elephantstudent <elephant.student@protonmail.com>
2020-05-26 20:53:48 +0000

    more info

--- script.py
+++ script.py
@@ -1,35 +1,89 @@
+import requests
+
+
 import matplotlib
 
 import matplotlib.pyplot as plt
 
-
 matplotlib.use('Agg')
-xAxis = list(range(-20,20))
 
 
-def parabola(xValues):
-
-  yValues = []
-
-  for x in xValues:
-
-    y = x*x
-
-    yValues.append(y)
-
-  return yValues
+import process
 
 
-yAxis = parabola(xAxis)
-sliceXAxis  = xAxis[5:35]
+def getImageFilename(options):
 
-sliceYAxis  = yAxis[5:35]
-style = 'ro'
+    startDate = options["from"]
 
-plt.plot(sliceXAxis , sliceYAxis , style)
+    endDate = options["to"]
 
-plt.axis([-10, 10, 0, 100])
+    days = getDailyMeasurements(startDate, endDate)
 
-filename = 'graph.png'
 
-plt.savefig(filename)
+    measurementName = options["metric"]
+
+    measurements = process.extract(days, measurementName)
+
+
+    if not options["isTotal"]:
+
+        measurements= process.dailyIncrease(measurements)
+
+
+    if not options["isRaw"]:
+
+        measurements= process.sevenDayAverage(measurements)
+
+
+    numMeasurements = len(measurements)
+
+    dayCounter = range(1, numMeasurements+1)
+
+    filename = saveImage(dayCounter, measurements)
+
+    return filename
+
+
+def getDailyMeasurements(startDate, endDate):
+
+    endpoint = 'https://api.covid19api.com/'
+
+    resource = 'total/country/united-states'
+
+    parameters = '?from=%s&to=%s' % (startDate, endDate)
+
+
+    response = requests.get(endpoint+resource+parameters)
+
+    return response.json()
+
+
+def saveImage(xValues, yValues):
+
+    dpi = 72
+
+    width = 640
+
+    height = 480
+
+    plt.figure(figsize=(width/dpi, height/dpi), dpi=dpi)
+
+
+    style = 'ro'
+
+    xMax = xValues[len(xValues)-1]
+
+    yMax = max(yValues)
+
+    plt.plot(xValues, yValues, style)
+
+    plt.axis([0, xMax, 0, yMax])
+
+
+    filename = 'graph.png'
+
+    plt.savefig(filename)
+
+    plt.clf()
+
+    return filename
